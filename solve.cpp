91792bfb58e591c306008c90c2dee53863e18a46
okan <okankoc@gmail.com>
2022-09-09 15:31:35 +0900

    Extend for solving roots of arbitrary functions

--- solve.cpp
+++ solve.cpp
@@ -1,30 +1,12 @@
 // Solve an equation of the form x*x = a.
 #include <iostream>
+#include <functional>
 
-const float learn_rate = 1e-3;
-const float tol = 1e-6;
+constexpr float learn_rate = 1e-3;
+constexpr float tol = 1e-6;
 
-float fun(const float &x) {
-  return x * x;
-}
-
-// TODO: Extend for arbitrary functions
-float grad(const float &x) {
-  return 2*x;
-}
-
-float optimize(const float &a) {
-  float x_soln = a;
-  float diff = 2*tol;
-  while (std::abs(diff) > tol) {
-    diff = learn_rate * grad(x_soln);
-    x_soln -= diff;
-  }
-  return x_soln;
-}
-
-float solve(const float &a) {
-  float x_soln = a;
+float solve(const std::function<float(float)> &fun, const std::function<float(float)> &grad, const float &x0, const float &a) {
+  float x_soln = x0;
   float diff = 2*tol;
   while (std::abs(diff) > tol) {
     diff = learn_rate * 2 * (fun(x_soln) - a) * grad(x_soln);
@@ -35,8 +17,10 @@ float solve(const float &a) {
 
 int main() {
 
-  const float a  = 5;
-  const float x_soln = solve(a);
+  const float a  = 7;
+  const std::function<float(float)> fun = [](const float &x) {return x*x*x;};
+  const std::function<float(float)> grad = [](const float &x) {return 3*x*x;};
+  const float x_soln = solve(fun, grad, 3.72, a);
   std::printf("Soln: x = %f, a = %f, f(x) = %f\n", x_soln, a, fun(x_soln));
   return 0;
 }
