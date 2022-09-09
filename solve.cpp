b043364c12ae7633bd5d99d9449ee8016bc4cb02
okan <okankoc@gmail.com>
2022-09-09 18:08:41 +0900

    Add multi-dim solver for root finding

--- solve.cpp
+++ solve.cpp
@@ -1,15 +1,19 @@
 // Solve an equation of the form x*x = a.
 #include <iostream>
 #include <functional>
+#include <Eigen/Dense>
 
 constexpr float learn_rate = 1e-3;
 constexpr float tol = 1e-6;
 
-float solve(const std::function<float(float)> &fun, const std::function<float(float)> &grad, const float &x0, const float &a) {
-  float x_soln = x0;
-  float diff = 2*tol;
-  while (std::abs(diff) > tol) {
-    diff = learn_rate * 2 * (fun(x_soln) - a) * grad(x_soln);
+Eigen::Vector2d solve(const std::function<Eigen::Vector2d(const Eigen::Vector2d&)> &fun,
+                      const std::function<Eigen::Matrix2d(const Eigen::Vector2d&)> &grad,
+                      const Eigen::Vector2d &x0,
+                      const Eigen::Vector2d &a) {
+  Eigen::Vector2d x_soln = x0;
+  auto diff = fun(x_soln) - a;
+  while (diff.norm() > tol) {
+    diff = learn_rate * 2 * grad(x_soln) * (fun(x_soln) - a);
     x_soln -= diff;
   }
   return x_soln;
@@ -17,10 +21,12 @@ float solve(const std::function<float(float)> &fun, const std::function<float(fl
 
 int main() {
 
-  const float a  = 7;
-  const std::function<float(float)> fun = [](const float &x) {return x*x*x;};
-  const std::function<float(float)> grad = [](const float &x) {return 3*x*x;};
-  const float x_soln = solve(fun, grad, 3.72, a);
-  std::printf("Soln: x = %f, a = %f, f(x) = %f\n", x_soln, a, fun(x_soln));
+  const Eigen::Vector2d a{2, 1};
+  const std::function<Eigen::Vector2d(const Eigen::Vector2d&)> fun = [](const Eigen::Vector2d &x) {Eigen::Vector2d out; out << x.dot(x), x[0] + x[1]; return out;};
+  const std::function<Eigen::Matrix2d(const Eigen::Vector2d&)> grad = [](const Eigen::Vector2d &x) {Eigen::Matrix2d out; out << 2*x[0], 1, 2*x[1], 1; return out;};
+  const auto x_soln = solve(fun, grad, Eigen::Vector2d{1, 1}, a);
+  std::cout << "Soln: x = " << x_soln;
+  std::cout << "\n a = " << a;
+  std::cout << "\n f(x) = " << fun(x_soln);
   return 0;
 }
